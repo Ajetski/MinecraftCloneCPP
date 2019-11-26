d15434d2087ea8d00d87f2a3c3da6f3481a6d65d
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2019-11-26 16:57:10 -0500

    add cubes with rotation

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]) {
 	// Set up some memory buffers for our display
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
 	// Set the window size
-	glutInitWindowSize(640, 640);
+	glutInitWindowSize(920, 920);
 	// Create the window with the title "Hello,GL"
 	glutCreateWindow("Hello, GL");
 	// Bind the two functions (above) to respond when necessary
@@ -42,34 +42,42 @@ int main(int argc, char* argv[]) {
 
 void init() {
 	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
 }
 
 void timer(int) {
-	//update(); //for animation
+	update(); //for animation
 	glutPostRedisplay();
 	glutTimerFunc(1000 / 60, timer, 0);
 }
 
 
 //control animation
-float x = -9;
+float z = -10;
 int state = 1;
+float ang = 0;
 
-/*
+
 void update() {
+
+	ang += 1.6;
+	if (ang > 360.0) {
+		ang = 0;
+	}
+	/*
 	if (state == 1) {
-		if (x < 9)
-			x += .1;
+		if (z < -5)
+			z += .1f;
 		else
 			state = -1;
 	}
 	else if (state == -1) {
-		if (x > -9)
-			x -= .1;
+		if (z > -12)
+			z -= .1f;
 		else
 			state = 1;
-	}
-}*/
+	}*/
+}
 
 void drawSquare() {
 	glBegin(GL_QUADS);
@@ -84,20 +92,55 @@ void drawSquare() {
 	glEnd();
 }
 
+
 void render() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 	glLoadIdentity();
 
-	//glTranslatef(x, x, 0.0);
+	glTranslatef(0.0, 0.0, -8.0);
+	glRotatef(ang, 1, 0, 0);
+	glRotatef(ang, 0, 1, 0);
+	glRotatef(ang, 0, 0, 1);
 	
 	//drawSquare();
 
 	glBegin(GL_QUADS);
-	glColor3f(0.0f, 1.0f, 0.7f);
-	glVertex2f(-1.0, 1.0);
-	glVertex2f(-1.0, -1.0);
-	glVertex2f(1.0, -1.0);
-	glVertex2f(1.0, 1.0);
+	//front
+	glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(-1.0, 1.0, 1.0);
+	glVertex3f(-1.0, -1.0, 1.0);
+	glVertex3f(1.0, -1.0, 1.0);
+	glVertex3f(1.0, 1.0, 1.0);
+	//back
+	glColor3f(0.0, 1.0, 0.0);
+	glVertex3f(1.0, 1.0, -1.0);
+	glVertex3f(1.0, -1.0, -1.0);
+	glVertex3f(-1.0, -1.0, -1.0);
+	glVertex3f(-1.0, 1.0, -1.0);
+	//right
+	glColor3f(0.0, 0.0, 1.0);
+	glVertex3f(1.0, 1.0, 1.0);
+	glVertex3f(1.0, -1.0, 1.0);
+	glVertex3f(1.0, -1.0, -1.0);
+	glVertex3f(1.0, 1.0, -1.0);
+	//left
+	glColor3f(1.0, 1.0, 0.0);
+	glVertex3f(-1.0, 1.0, -1.0);
+	glVertex3f(-1.0, -1.0, -1.0);
+	glVertex3f(-1.0, -1.0, 1.0);
+	glVertex3f(-1.0, 1.0, 1.0);
+	//top
+	glColor3f(0.0, 1.0, 1.0);
+	glVertex3f(-1.0, 1.0, -1.0);
+	glVertex3f(-1.0, 1.0, 1.0);
+	glVertex3f(1.0, 1.0, 1.0);
+	glVertex3f(1.0, 1.0, -1.0);
+	//bottom
+	glColor3f(1.0, 0.0, 1.0);
+	glVertex3f(-1.0, -1.0, -1.0);
+	glVertex3f(-1.0, -1.0, 1.0);
+	glVertex3f(1.0, -1.0, 1.0);
+	glVertex3f(1.0, -1.0, -1.0);
 	glEnd();
 
 	
@@ -109,7 +152,7 @@ void reshape(int w, int h) {
 	glViewport(0, 0, (GLsizei) w, (GLsizei) h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective();
+	gluPerspective(60.0, 1.0, 1.0, 15.0);
 	glMatrixMode(GL_MODELVIEW);
 }
 
