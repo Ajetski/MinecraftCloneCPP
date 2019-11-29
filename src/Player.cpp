22cf4d22b94fc1da01c689eb86e207e1b749a693
Ajetski <ajeniski4@gmail.com>
2019-11-29 00:04:55 -0500

    init player class

---
+++ src/Player.cpp
@@ -0,0 +1,47 @@
+#include "Player.hpp"
+#include <GL\glew.h>
+#include <iostream>
+
+void Player::setCamera() {
+	glLoadIdentity();
+	glTranslatef(pos_x, pos_y, pos_z);
+	glRotatef(rot_pitch, 1, 0, 0);
+	glRotatef(rot_yaw, 0, 1, 0);
+	glRotatef(rot_roll, 0, 0, 1);
+}
+
+void Player::setPos(float x, float y, float z) {
+	pos_x = x;
+	pos_y = y;
+	pos_z = z;
+}
+
+void Player::setRot(float pitch, float yaw, float roll) {
+	rot_pitch = pitch;
+	rot_yaw = yaw;
+	rot_roll = roll;
+}
+
+const float& Player::x() const {
+	return pos_x;
+}
+
+const float& Player::y() const {
+	return pos_y;
+}
+
+const float& Player::z() const {
+	return pos_z;
+}
+
+const float& Player::pitch() const {
+	return rot_pitch;
+}
+
+const float& Player::roll() const {
+	return rot_roll;
+}
+
+const float& Player::yaw() const {
+	return rot_yaw;
+}
---
+++ src/Player.hpp
@@ -0,0 +1,15 @@
+class Player {
+public:
+	void setCamera();
+	void setPos(float x, float y, float z);
+	void setRot(float pitch, float yaw, float roll);
+	const float& x() const;
+	const float& y() const;
+	const float& z() const;
+	const float& pitch() const;
+	const float& roll() const;
+	const float& yaw() const;
+private:
+	float pos_x = 0, pos_y = 2, pos_z = 0;
+	float rot_pitch = 0, rot_yaw = 0, rot_roll = 0;
+};
